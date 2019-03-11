32c4b90313f4460d0baab9e9a62d5d4723962f79
Pavel <fire_ra@bk.ru>
2019-03-11 09:21:44 +0300

    first commit study how its all work

---
+++ app/src/main/java/com/vk/directop/myappforhub/MainActivity.java
@@ -0,0 +1,19 @@
+package com.vk.directop.myappforhub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public int sunFrom1ToN(int n){
+        return 0;
+    }
+
+}
---
+++ app/src/main/java/com/vk/directop/myappforhub/User.java
@@ -0,0 +1,17 @@
+package com.vk.directop.myappforhub;
+
+import java.util.Map;
+
+public class User{
+    private int id;
+    private String name;
+    private int age;
+    private Sex sex;
+    private static Map<Integer, User> allUsers;
+
+    public class Sex{
+        private String sex;
+
+    }
+
+}
